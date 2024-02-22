d2d0e9bd0f6440e97802e9bd67a8ec7ce723e9b5
Jeremy Caris <jeremy@714web.com>
2024-02-22 08:44:34 -0500

    Update gfcombinedformentries.php

--- gfcombinedformentries.php
+++ gfcombinedformentries.php
@@ -54,7 +54,7 @@ require 'inc/plugin-update-checker-5.3/plugin-update-checker.php';
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
 $myUpdateChecker = PucFactory::buildUpdateChecker(
-    'https://github.com/jeremycaris/gf-combined-form-entries/',
+    'https://github.com/714Web/gf-combined-form-entries/',
     __FILE__,
     'gf-combined-form-entries'
 );
